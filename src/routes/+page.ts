765e37054cb85880d5dac5fa5465bee73673958e
navelist <navelist89@gmail.com>
2023-04-01 19:26:09 +0900

    Removed previous svelte tutorial pages

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,3 +1,18 @@
+
 // since there's no dynamic data here, we can prerender
 // it so that it gets served as a static asset in production
 export const prerender = true;
+
+import RECIPES from "../../data/recipes.json";
+import IMAGES from "../../data/images.json";
+import { RawRecipe } from "../scripts/types";
+
+
+/** @type {import('./$types').PageLoad} */
+export function load() {
+    return {
+        recipes : RECIPES.filter((recipe: RawRecipe) => !recipe.isAlternate),
+        alternateRecipes : RECIPES.filter((recipe: RawRecipe) => recipe.isAlternate),
+        images: IMAGES
+    }
+}
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -16,18 +16,8 @@
 			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
 		</svg>
 		<ul>
-			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
-				<a href="/">Home</a>
-			</li>
-			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
-				<a href="/about">About</a>
-			</li>
-			<li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
-				<a href="/sverdle">Sverdle</a>
-			</li>
-
-			<li aria-current={$page.url.pathname.startsWith('/recipe') ? 'page' : undefined}>
-				<a href="/recipe">Recipe</a>
+			<li aria-current={$page.url.pathname.startsWith('/') ? 'page' : undefined}>
+				<a href="/">Recipe</a>
 			</li>
 			<li aria-current={$page.url.pathname.startsWith('/top-down') ? 'page' : undefined}>
 				<a href="/top-down">TOP-DOWN</a>
